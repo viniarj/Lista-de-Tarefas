05f30a68839c00119b9fae3a0a1e8dce15e9832b
viniarj <dvdvinicios@gmail.com>
2024-04-21 15:08:52 -0300

    feat: validation

---
+++ tasks/static/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    var deleteBtn = $('.delete-btn');
+    var searchBtn = $('#search-btn');
+    var searchForm = $('#search-form');
+
+    $(deleteBtn).on('click', function(e) {
+
+        e.preventDefault();
+
+        var delLink = $(this).attr('href');
+        var result = confirm('quer deletar esta tarefa?');
+
+        if(result) {
+            window.location.href = delLink;
+        }
+    });
+
+    $(searchBtn).on('click', function() {
+        searchForm.submit();
+    });
+
+});
---
+++ tasks/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('ola/', views.ola, name='ola'),
+    path('', views.taskList, name='task-list'),
+    path('yourname/<str:name>', views.yourName, name='your-name'),
+    path('newtask/', views.newTask, name="task-view"),
+    path('edit/<int:id>', views.editTask, name='edit-task'),
+    path('delete/<int:id>', views.deleteTask, name='delete-task'),
+    path('task/<int:id>',views.taskView, name='task'),
+]
